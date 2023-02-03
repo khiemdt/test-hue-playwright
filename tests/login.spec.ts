89d8ccf294ec3194f04bd6a36a5c318400549068
khiemdt <khiemdt.it@tripi.vn>
2023-02-04 00:11:29 +0700

    push code

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,18 +1,24 @@
-import { test, expect, type Page } from '@playwright/test';
-import { useNamePassCase } from './test';
+import { test, expect, type Page } from "@playwright/test";
+import { useNamePassCase } from "./test";
 
-for (const { sdt, pass, result } of useNamePassCase) {
-    test(`Search function test ${sdt} `, async ({ page }: { page: Page }) => {
-        // Navigate to the website
-        await page.goto('https://alpha.tripi.vn/');
-        const loginButton: any = await page.getByRole('button', { name: 'Đăng nhập' });
-        await loginButton?.click();
-        await expect(page.getByText('Đăng nhập Tripi Partner qua')).toBeVisible();
-        await page.getByPlaceholder('Ví dụ: 0901234567').click();
-        await page.getByPlaceholder('Ví dụ: 0901234567').fill(sdt);
-        await page.getByPlaceholder('******').click();
-        await page.getByPlaceholder('******').fill(pass);
-        await page.getByRole('button', { name: 'Đăng nhập' }).click();
-        await expect(page.getByText(result)).toBeVisible();
+for (const { sdt, pass, result, id } of useNamePassCase) {
+  test(`Search function test ${result} `, async ({ page }: { page: Page }) => {
+    //
+    await page.goto("https://alpha.tripi.vn/");
+    const loginButton: any = await page.getByRole("button", {
+      name: "Đăng nhập",
     });
+    await loginButton?.click();
+    await expect(page.getByText("Đăng nhập Tripi Partner qua")).toBeVisible();
+    await page.getByPlaceholder("Ví dụ: 0901234567").click();
+    await page.getByPlaceholder("Ví dụ: 0901234567").fill(sdt);
+    await page.getByPlaceholder("******").click();
+    await page.getByPlaceholder("******").fill(pass);
+    await page.getByRole("button", { name: "Đăng nhập" }).click();
+    await expect(page.getByText(result)).toBeVisible();
+    await page.screenshot({
+        path: `ảnh ${id}.png`,
+        fullPage: false,
+      });
+  });
 }
--- tests/test.ts
+++ tests/test.ts
@@ -1,51 +1,60 @@
 interface UserInfo {
-    sdt?: any;
-    pass?: any;
-    result?: any;
+  sdt?: any;
+  pass?: any;
+  result?: any;
+  id?: any;
 }
 
 export const searchQueries = [
-    {
-        search: 'bún',
-        searchQuerie: 10,
-    },
-    {
-        search: 'cá',
-        searchQuerie: 10,
-    },
-    {
-        search: '@#@#',
-        searchQuerie: 'Không tìm thấy sản phẩm nào',
-    },
-    {
-        search: 'giò lụa',
-        searchQuerie: 3,
-    },
+  {
+    search: "bún",
+    searchQuerie: 10,
+  },
+  {
+    search: "cá",
+    searchQuerie: 10,
+  },
+  {
+    search: "@#@#",
+    searchQuerie: "Không tìm thấy sản phẩm nào",
+  },
+  {
+    search: "giò lụa",
+    searchQuerie: 3,
+  },
 ];
 
 export const useNamePassCase: UserInfo[] = [
-    {
-        sdt: '0854328940',
-        pass: '123456',
-        result: 'Gửi phản hồi',
-    },
-    {
-        sdt: '2342412312',
-        pass: '123456',
-        result: 'Số điện thoại 2342412312 không hợp lệ',
-    },
-    {
-        sdt: '0854328940',
-        pass: '123',
-        result: 'Mật khẩu không hợp lệ',
-    },
+  {
+    id: 1,
+    sdt: "0854328940",
+    pass: "123456",
+    result: "Gửi phản hồi",
+  },
+  {
+    id: 2,
+    sdt: "2342412312",
+    pass: "123456",
+    result: "Số điện thoại 2342412312 không hợp lệ",
+  },
+  {
+    id: 3,
+    sdt: "0854328940",
+    pass: "123",
+    result: "Mật khẩu không hợp lệ",
+  },
+  {
+    id: 4,
+    sdt: "0854328940",
+    pass: "123",
+    result: "Mật khẩu không hợp lệ 1234",
+  },
 ];
 
 export async function setCookieVals() {
+  const cookies = [
+    { name: "ACCESS_TOKEN", value: "33b3dc0b-81f5-4ba8-bbe5-e8b94fc71312" },
+  ];
 
-    const cookies = [
-        {name:"ACCESS_TOKEN", value:"33b3dc0b-81f5-4ba8-bbe5-e8b94fc71312"},
-    ]
-
-    return cookies;
+  return cookies;
 }
